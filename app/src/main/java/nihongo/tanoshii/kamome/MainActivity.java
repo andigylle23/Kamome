4a5b3b0a64ad8362b5973982ca8f347ceca48e24
kyrie <kyrie@isr.com.ph>
2018-10-19 21:41:49 +0800

    First commit

---
+++ app/src/main/java/nihongo/tanoshii/kamome/MainActivity.java
@@ -0,0 +1,20 @@
+package nihongo.tanoshii.kamome;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+}
